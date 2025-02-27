fc3ad2eded2353bd7f56585396214bd8ec86b666
Jean Staicy Auma Otambo <jeanstaicy21@gmail.com>
2025-02-27 23:35:11 +0300

    batman

---
+++ fraud-blocker/src/App.jsx
@@ -0,0 +1,22 @@
+import { Button } from './components/ui/button'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Fraud Blocker</h1>
+      <div className="card">
+        <p>
+        Secure your transactions with AI-driven fraud detection.
+        </p>
+
+        <Button>Get Started</Button>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
